e3c1c21b95c2767e7ca6169c5765ab2d352b4810
tainamorais <tainafms@gmail.com>
2023-04-06 16:03:05 -0300

    Requisito 02 concluído.

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+// REQ. 02
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+'User', 
+{
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+{
+    underscored: true,
+    timestamps: false,
+    tableName: 'users',
+  },
+);
+
+  return User;
+};

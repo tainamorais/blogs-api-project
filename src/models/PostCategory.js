ce1aeeb18a9591a7e10f94e689955a4fa559e512
tainamorais <tainafms@gmail.com>
2023-04-07 10:30:11 -0300

    Requisito 11 concluído.

---
+++ src/models/PostCategory.js
@@ -0,0 +1,24 @@
+// REQ. 11
+
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    'PostCategory',
+    { postId: { type: DataTypes.INTEGER }, categoryId: { type: DataTypes.INTEGER } },
+    { timestamps: false, tableName: 'posts_categories', underscored: true },
+  );
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, { as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, { as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
